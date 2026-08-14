0082ac9bbb971b4b1d9a6a197c6423a6773be358
madebyRohitjha <rohit.jha.ic@gmail.com>
2026-08-14 16:40:09 +0900

    data analysis

--- train.py
+++ train.py
@@ -6,3 +6,15 @@ from tensorflow.keras.datasets import mnist
 
 print(X_train.shape)
 print(X_test.shape)
+
+print(y_train[0])
+
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+print(X_train.shape)
+print(X_test.shape)
+
+print(y_train[0])
