c9af3051b8808624654d84edcf27369feabb3447
madebyRohitjha <rohit.jha.ic@gmail.com>
2026-08-15 16:41:40 +0900

    data visualization

--- train.py
+++ train.py
@@ -18,3 +18,10 @@ print(X_train.shape)
 print(X_test.shape)
 
 print(y_train[0])
+
+import matplotlib.pyplot as plt
+
+plt.imshow(X_train[0], cmap="gray")
+plt.title(f"Label: {y_train[0]}")
+plt.axis("off")
+plt.show()
