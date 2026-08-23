ed9d601b683da4a190de60657ac2c9d447486bc6
madebyRohitjha <rohit.jha.ic@gmail.com>
2026-08-23 15:18:45 +0900

    Load trained model and predict first MNIST digit

--- predict.py
+++ predict.py
@@ -1,5 +1,18 @@
 import tensorflow as tf
 
+from tensorflow.keras.datasets import mnist
+
 model = tf.keras.models.load_model("mnist_ann.keras")
 
 print("Model loaded successfully!")
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_test = X_test / 255.0
+
+prediction = model.predict(X_test[0].reshape(1, 28, 28))
+
+predicted_digit = prediction.argmax()
+
+print("Predicted Digit:", predicted_digit)
+print("Actual Digit:", y_test[0])
