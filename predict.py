26cb0aaa3762f6bde9754a1f4238f6d5453b602d
madebyRohitjha <rohit.jha.ic@gmail.com>
2026-08-29 16:27:22 +0900

    Calculate prediction accuracy on random MNIST samples

--- predict.py
+++ predict.py
@@ -1,3 +1,4 @@
+correct = 0
 import random
 import tensorflow as tf
 
@@ -38,3 +39,21 @@ for i in range(5):
     print("Prediction:", predicted_digit)
     print("Actual:", y_test[image_index])
     print(f"Confidence: {confidence:.2f}%")
+
+    for i in range(100):
+    image_index = random.randint(0, 9999)
+
+    prediction = model.predict(
+        X_test[image_index].reshape(1, 28, 28),
+        verbose=0
+    )
+
+    predicted_digit = prediction.argmax()
+
+    if predicted_digit == y_test[image_index]:
+        correct += 1
+
+accuracy = (correct / 100) * 100
+
+print(f"Correct Predictions: {correct}/100")
+print(f"Accuracy: {accuracy:.2f}%")
